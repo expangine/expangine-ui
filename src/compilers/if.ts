37f4e0362307c8e306869cad2aff516fff5588ac
Philip Diffenderfer <pdiffenderfer@gmail.com>
2020-10-01 18:08:30 -0400

    Fix createIf boolean requirement

--- src/compilers/if.ts
+++ src/compilers/if.ts
@@ -1,7 +1,8 @@
+import { BooleanOps } from 'expangine-runtime';
 import { NodeCompiler } from '../Node';
 import { CompilerSwitch } from './switch';
 
 
 export const CompilerIf: NodeCompiler = ([tag, attrs, events, childSlots], component, scope, parent) => 
-  CompilerSwitch([tag, { value: true, ...attrs }, events, childSlots], component, scope, parent)
+  CompilerSwitch([tag, { value: true, isEqual: BooleanOps.isEqual, ...attrs }, events, childSlots], component, scope, parent)
 ;
